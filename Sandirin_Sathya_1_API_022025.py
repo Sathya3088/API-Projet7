1f54a8ce757aca576663b002c56e4223520d2ee1
Sathya3088 <shanow@hotmail.fr>
2025-03-10 14:42:37 +0100

    Update Sandirin_Sathya_1_API_022025.py
    ajout shap.values

--- Sandirin_Sathya_1_API_022025.py
+++ Sandirin_Sathya_1_API_022025.py
@@ -2,9 +2,19 @@ from joblib import load
 from fastapi import FastAPI
 import pandas as pd
 import numpy as np
+import shap
+from fastapi.middleware.cors import CORSMiddleware
 
 dataframe = pd.read_csv('data/df.csv')
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Chargement du modèle :
 model = load("model.joblib")
@@ -25,7 +35,15 @@ def predict(client_id: int):
     predicted_probability = prediction[0][1]
     decision = "Crédit accepté" if predicted_probability >= threshold else "Crédit refusé"
 
-    return {"client_id": client_id, "probability": predicted_probability, "decision" : decision}
+    # Récupérer les valeurs SHAP pour le client
+    client_features = client_data.drop(['TARGET', 'SK_ID_CURR', 'index'], axis=1)
+    assert client_features.shape[1] == 795, "Le nombre de features doit être de 795."
+
+    explainer = shap.Explainer(model)
+    shap_values = explainer(client_features.values)
+    shap_values_list = shap_values.values.tolist()
+
+    return {"client_id": client_id, "probability": predicted_probability, "decision" : decision, "shap_values": shap_values_list}
 
 
 
